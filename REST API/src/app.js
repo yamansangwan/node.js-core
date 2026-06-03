c003c36ebaaced52553512d95fc1a1bfea0f4b11
Yaman Sangwan <yamansangwan.cse@gmail.com>
2026-06-03 11:18:50 +0530

    Implement CRUD operations for tweets in app.js

--- REST API/src/app.js
+++ REST API/src/app.js
@@ -1,11 +1,54 @@
 // used to create the server
 
-const exp = require("express");
+const express = require("express")
 
-const app = exp();
+const app = express()
 
-app.post("/notes" , (req,res) => {
-    console.log(req.body)
+app.use(express.json())
+
+let tweets = [];
+
+
+
+// posting the tweets
+app.post("/tweets" , (req,res) => {
+    tweets.push(req.body)
+    res.status(201).json({msg : "Success Tweet Generated"})
 })
+
+
+
+
+// getting the tweets 
+app.get("/tweets" , (req,res) => {
+    res.status(200).json({
+        tweets : tweets
+    })
+})
+
+
+
+// deleting a tweet
+app.delete("/tweets/:index", (req,res) => {
+    const index = req.params.index;
+    tweets.splice(index,1);
+    res.status(200).json({
+        msg : "deleted successfully"
+    })
+})
+
+
+
+// updating a tweet
+app.patch("/tweets/:index" , (req,res) => {
+    const index = req.params.index;
+    const tweet = req.body.tweet;
+    tweets[index].tweet = tweet;
+
+    res.status(200).json({
+        msg : "updated successfully"
+    })
+})
+
 
 module.exports = app;
