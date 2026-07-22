3a13349d8535b2b9ee102a117fa2e5ed53466189
Yaman Sangwan <yamansangwan.cse@gmail.com>
2026-07-22 17:40:42 +0530

    Update MongoDB connection credentials

--- 06-ImageKit-Upload+DB/src/db/db.js
+++ 06-ImageKit-Upload+DB/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 async function connectDB() {
-    await mongoose.connect("mongodb+srv://superadmin:superadmin123@backend-db.f2tspsz.mongodb.net/winpostDB")
+    await mongoose.connect("mongodb+srv://test:test123@backend-db.f2tspsz.mongodb.net/winpostDB")
 }
 
 module.exports = connectDB
