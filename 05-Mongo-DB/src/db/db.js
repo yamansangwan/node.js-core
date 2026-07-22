00e3be789657174ecd677b9404ff175ad5a37c7a
Yaman Sangwan <yamansangwan.cse@gmail.com>
2026-07-22 18:02:25 +0530

    Update MongoDB connection credentials in db.js

--- 05-Mongo-DB/src/db/db.js
+++ 05-Mongo-DB/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDb (){
 
-    await mongoose.connect("mongodb+srv://superadmin:superadmin123@backend-db.f2tspsz.mongodb.net/newDB?appName=backend-db")
+    await mongoose.connect("mongodb+srv://dummy:dummy123@backend-db.f2tspsz.mongodb.net/newDB?appName=backend-db")
     console.log("Connected to DB");
     
 }
